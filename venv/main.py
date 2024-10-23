548751c5b07547b0e1e5f8a73d168ebc18267c03
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-10-22 23:43:29 -0700

    importing libraries, defining params

---
+++ venv/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+company = 'FB'
+start = dt.datetime(2012,1,1)
+end = dt.datetime(2024,1,1)
+
+data = web.DataReader(company, 'yahoo', start, end)
